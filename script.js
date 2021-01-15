4ebbeba42709728e6369da8dd53ec81d0088aa80
Julia Medeiros Guimaraes <juliamedeiros@ice.ufjf.br>
2021-01-14 21:54:52 -0300

    Adaptação para estilo bonitinho

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 let canvas = document.querySelector("canvas");
-canvas.width = 640;
-canvas.height = 480;
-
+canvas.width = 960;
+canvas.height = 720;
 
 let contexto = canvas.getContext("2d");
                 // posição 0, 0 (meio) de tamanho max (largura x altura)
 contexto.fillRect (0, 0, canvas.width, canvas.height);
 
 contexto.fillStyle = "white";
-                // posição 310, 230 (meio) de tamanho 20 x 20
-contexto.fillRect(310,230, 20, 20);
+                // posição 470, 350 (meio) de tamanho 20 x 20
+contexto.fillRect(470,350, 20, 20);
