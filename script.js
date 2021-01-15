bc2b441b926876df913be69fe201c78e8876d179
Julia Medeiros Guimaraes <juliamedeiros@ice.ufjf.br>
2021-01-15 19:45:57 -0300

    +Funcao de atualização dos frames

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+// CRIAÇÃO DO CANVAS
 let canvas = document.querySelector("canvas");
 canvas.width = 960;
 canvas.height = 720;
 
+// POSIÇÃO DO QUADRADINHO
+let posX = 470;
+let posY = 350;
+
+// DEFINIÇÃO DE CONTEXTO
 let contexto = canvas.getContext("2d");
                 // posição 0, 0 (meio) de tamanho max (largura x altura)
 contexto.fillRect (0, 0, canvas.width, canvas.height);
 
-contexto.fillStyle = "white";
-                // posição 470, 350 (meio) de tamanho 20 x 20
-contexto.fillRect(470,350, 20, 20);
+frame();
+
+function frame()
+{
+    //Cria fundo preto para limpar
+    contexto.fillStyle = "black";
+    contexto.fillRect (0, 0, canvas.width, canvas.height);
+
+    //Branco
+    contexto.fillStyle = "white";
+    contexto.fillRect(posX, posY, 20, 20);
+}
+
+
