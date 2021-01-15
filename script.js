189d986fc4cfbfce66ff4103d27bb98d41c1acbc
Julia Medeiros Guimaraes <juliamedeiros@ice.ufjf.br>
2021-01-15 20:00:15 -0300

    +Animação (requestAnimationFrame)

--- script.js
+++ script.js
@@ -3,19 +3,25 @@ let canvas = document.querySelector("canvas");
 canvas.width = 960;
 canvas.height = 720;
 
-// POSIÇÃO DO QUADRADINHO
+// INFORMAÇÕES DO QUADRADINHO
 let posX = 470;
 let posY = 350;
+let t0;
+let dt;
 
 // DEFINIÇÃO DE CONTEXTO
 let contexto = canvas.getContext("2d");
                 // posição 0, 0 (meio) de tamanho max (largura x altura)
 contexto.fillRect (0, 0, canvas.width, canvas.height);
 
-frame();
+requestAnimationFrame(frame);
 
-function frame()
+function frame(t)
 {
+    //Colocar em função do tempo e nao do fps
+    t0 = t0 ?? t;
+    dt = (t - t0)/1000;
+
     //Cria fundo preto para limpar
     contexto.fillStyle = "black";
     contexto.fillRect (0, 0, canvas.width, canvas.height);
@@ -23,6 +29,10 @@ function frame()
     //Branco
     contexto.fillStyle = "white";
     contexto.fillRect(posX, posY, 20, 20);
+
+    //Request next
+    requestAnimationFrame(frame);
+    t0 = t;
 }
 
 
